a1620ca101dc687096dbb1eab857a2d712271ecb
alansabumathew <alanmathew349@gmail.com>
2026-03-17 08:41:29 +0530

    Updated footer, and member data as of 2026

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 // Making Navbar Responsive
-$(document).ready(function(){
-    $(".hamburger i").click(function(){
-        $(this).toggleClass("fa-times");
-        $(".nav-links").toggleClass("mobile-view");
-    });
+$(document).ready(function () {
+  $('.hamburger i').click(function () {
+    $(this).toggleClass('fa-times');
+    $('.nav-links').toggleClass('mobile-view');
+  });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('footer.html')
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById('footer').innerHTML = data;
+
+      // set current year
+      const year = document.getElementById('year');
+      if (year) {
+        year.textContent = new Date().getFullYear();
+      }
+    })
+    .catch((error) => console.error('Error loading footer:', error));
+});
